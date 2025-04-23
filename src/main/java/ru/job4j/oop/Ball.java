e7e0b01790c1ad880b95569dd81be7e1298655d4
Sergey <92508690+SN737@users.noreply.github.com>
2025-04-23 11:25:10 +0300

    тренировка

--- src/main/java/ru/job4j/oop/Ball.java
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -1,6 +1,13 @@
 package ru.job4j.oop;
 
 public class Ball {
+
+    public String name;
+
+    public Ball(String name) {
+        this.name = name;
+    }
+
     public void tryRun(boolean condition) {
         if (condition) {
             System.out.println("Колобок съеден");
--- src/main/java/ru/job4j/oop/BallStory.java
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -2,7 +2,7 @@ package ru.job4j.oop;
 
 public class BallStory {
     public static void main(String[] args) {
-        Ball ball = new Ball();
+        Ball ball = new Ball("Колоб");
         Wolf wolf = new Wolf();
         Fox fox = new Fox();
         Hare hare = new Hare();
@@ -11,7 +11,7 @@ public class BallStory {
         wolf.tryEat(ball);
         System.out.println("Колобка пытается съесть заяц");
         hare.tryEat(ball);
-        System.out.println("Колобка пытается съесть лиса..");
+        System.out.println(ball.name + "а пытается съесть лиса..");
         fox.tryEat(ball);
 
         System.out.println("Ну тогда волк  пытается съесть зайца");
