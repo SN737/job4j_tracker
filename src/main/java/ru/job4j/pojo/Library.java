5b03713f79a01ebe5be1997a1462d807786cc2e5
Sergey <92508690+SN737@users.noreply.github.com>
2025-04-26 11:26:46 +0300

    3. Массивы и модели. [#174892 #541228]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -2,24 +2,24 @@ package ru.job4j.pojo;
 
 public class Library {
     public static void main(String[] args) {
-    Book book1 = new Book("Clean Code", 464);
-    Book book2 = new Book("Designing Data-Intensive Apps", 640);
-    Book book3 = new Book("Learning Java", 500);
-    Book book4 = new Book("Think Java", 440);
+        Book book1 = new Book("Clean Code", 464);
+        Book book2 = new Book("Designing Data-Intensive Apps", 640);
+        Book book3 = new Book("Learning Java", 500);
+        Book book4 = new Book("Think Java", 440);
 
-    Book[] books = new Book[4];
-    books[0] = book1;
-    books[1] = book2;
-    books[2] = book3;
-    books[3] = book4;
+        Book[] books = new Book[4];
+        books[0] = book1;
+        books[1] = book2;
+        books[2] = book3;
+        books[3] = book4;
 
         for (int i = 0; i < books.length; i++) {
             System.out.println(books[i].getName());
-                }
-            swap(0, 3, books);
+        }
+        swap(0, 3, books);
 
         for (int i = 0; i < books.length; i++) {
-            if (books[i].equals(book1)) {
+            if (books[i].getName().equals("Clean Code")) {
                 System.out.println(books[i].getName());
             }
         }
