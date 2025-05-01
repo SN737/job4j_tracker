324962d9fe3cbad502e1e86a3c8017626950ea82
Sergey <92508690+SN737@users.noreply.github.com>
2025-05-01 14:34:22 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                result = i;
+            }
+        } if (result == -1) {
+            throw new ElementNotFoundException("Element not found");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] array = {"c", "d", "e", "f", "g", "h", "i", "j", "k"};
+        try {
+            indexOf(array, "a");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
