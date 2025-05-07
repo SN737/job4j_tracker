f148fdca217634bc953fcf038209060e039bb421
Sergey <92508690+SN737@users.noreply.github.com>
2025-05-07 11:15:30 +0300

    sandbox

---
+++ src/main/java/ru/job4j/sandbox/DownClass.java
@@ -0,0 +1,10 @@
+package ru.job4j.sandbox;
+
+public interface DownClass   {
+
+    void takeShot();
+
+    public static String takeOver() {
+        return "This TakeOver-2";
+    }
+}
---
+++ src/main/java/ru/job4j/sandbox/SandBox.java
@@ -0,0 +1,17 @@
+package ru.job4j.sandbox;
+
+public class SandBox {
+
+    public void method() {
+
+    }
+
+    public  static void takeOver() { }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
