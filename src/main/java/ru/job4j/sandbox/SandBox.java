a9515361bbf334c114f2ee8ad93fd43c6d584776
Sergey <92508690+SN737@users.noreply.github.com>
2025-05-08 17:41:31 +0300

    BubbleSort

--- src/main/java/ru/job4j/sandbox/SandBox.java
+++ src/main/java/ru/job4j/sandbox/SandBox.java
@@ -1,15 +1,37 @@
 package ru.job4j.sandbox;
 
 public class SandBox {
+    public static int[] array = {5,3,4,2,1,1,6,9,11,14,12,10};
 
-    public void method() {
+    public static int[] bubble(int [] arr) {
 
+        for (int j = 0; j < arr.length - 1; j++ ) {
+            if (arr[j] >= arr[j+1]) {
+                for (int i = 0; i < arr.length - 1; i++) {
+                    if (arr[i] > arr[i+1]) {
+                        int temp = arr[i];
+                        arr[i] = arr[i+1];
+                        arr[i+1] = temp;
+                    }
+                }
+                j=0;
+            }
+        }
+        return arr;
     }
 
-    public  static void takeOver() { }
+    public static  void arrayPrint(int[] arrayToPrint) {
+        for (int item : arrayToPrint) {
+            System.out.print(item + ",");
+        }
+    }
 
-    public static void main(String[] args) {
 
+public static void main(String[] args) {
+    System.out.println("было:");
+    arrayPrint(array);
+    System.out.println();
+    arrayPrint(bubble(array));
     }
 }
 
