4739d49c8ad7398c564c6b8e605ac31fbc3072f6
Sergey <92508690+SN737@users.noreply.github.com>
2025-05-10 11:23:12 +0300

    sandbox

--- src/main/java/ru/job4j/sandbox/SandBox.java
+++ src/main/java/ru/job4j/sandbox/SandBox.java
@@ -1,7 +1,10 @@
 package ru.job4j.sandbox;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class SandBox {
-    public static  int[] array = {5, 3, 4, 2, 1, 1, 6, 9, 11, 14, 12, 10};
+    public static  int[] array = {25, 3, 4, 2, 1, -1, 6, -999, 9, 11, 14, 12, 10};
 
     public static int[] bubble(int[] arr) {
         for (int j = 0; j < arr.length - j; j++) {
@@ -12,6 +15,7 @@ public class SandBox {
                     arr[i + 1] = temp;
                 }
             }
+            System.out.println(Arrays.toString(arr));
         }
         return arr;
     }
