63b88820d79e3b2fb385f098098b2def124cd099
Sergey <92508690+SN737@users.noreply.github.com>
2025-05-11 22:26:18 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -1,1 +1,16 @@
 
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("canuck8@gmail.com",  "Connor Garland");
+        map.put("canuck6@gmail.com",  "Brock Boeser");
+        map.put("canuck17@gmail.com",  "Philipp Hronek");
+        for (String key : map.keySet()) {
+            System.out.println(key);
+        }
+    }
+}
