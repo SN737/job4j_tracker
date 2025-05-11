64b91a1c11df050c49b168e176b2698df9130114
Sergey <92508690+SN737@users.noreply.github.com>
2025-05-11 23:46:48 +0300

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,52 @@
+package ru.job4j.bank;
+
+import ru.job4j.bank.Account;
+import ru.job4j.bank.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        if (!users.containsKey(user.getPassport())) {
+            users.putIfAbsent(user, new ArrayList<Account>());
+        }
+    }
+
+    public void deleteUser(String passport) {
+        users.remove(users.get(passport));
+    }
+
+    public void addAccount(String passport, Account account) {
+
+
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        return null;
+    }
+
+    public boolean transferMoney(String sourcePassport, String sourceRequisite,
+                                 String destinationPassport, String destinationRequisite,
+                                 double amount) {
+        boolean result = false;
+        return result;
+    }
+
+    public List<Account> getAccounts(User user) {
+        return users.get(user);
+    }
+}
